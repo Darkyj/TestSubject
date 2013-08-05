848db325029dabeed7a77540fc5a1b5a3a9213b4
Josue <josh.rojas25@gmail.com>
2013-08-04 21:38:32 -0700

    Recommiting

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-phpinfo();
+$jama = array('lol' => 5,
+			   'he' => 4 );
+
+foreach ($jama as $key => $value) {
+	echo $key . " " . $value . " ";
+}
+
+
+
+
+$jama2 = array(array('lol',1,2,3),array(1,2,3,4),array(1,3,2,1));
+
+echo $jama2[0][3];
+
+echo "Hello Jr!!!!!!s";
+
 
 
 
