665d3d61180e4d6d9881d440e1cf32772bb3539b
Josue <josh.rojas25@gmail.com>
2013-08-04 21:36:46 -0700

    ugh

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
-$jama = array('lol' => 5,
-			   'he' => 4 );
-
-foreach ($jama as $key => $value) {
-	echo $key . " " . $value . " ";
-}
-
-
-
-
-$jama2 = array(array('lol',1,2,3),array(1,2,3,4),array(1,3,2,1));
-
-echo $jama2[0][3];
-
-echo "Hello Jr!!!!!!";
-
+phpinfo();
 
 
 
